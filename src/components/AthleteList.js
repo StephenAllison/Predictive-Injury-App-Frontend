02af4cfbea7ff453eee2b23efe91429a09a3f408
Stephen Allison <stephen.allison.technology@gmail.com>
2018-10-12 09:19:21 -0400

    trying cards

--- src/components/AthleteList.js
+++ src/components/AthleteList.js
@@ -54,26 +54,29 @@ class AthleteList extends Component {
             {this.state.listOfAthletes &&
               this.state.listOfAthletes.map((athlete, index) => {
                 return (
-                  <CardDeck>
-                    <Card>
+                  <div>
+                    <div>
                       <div key={athlete._id}>
                         <Link to={`/athletes/${athlete._id}`}>
+                          {athlete.currentInjuryStatus}
                           <img src={athlete.imgPath} />
-                          <CardBody>
+                          <div>
                             {athlete.currentInjuryStatus}
-                            <CardTitle>{athlete.name}</CardTitle>
-                            <CardSubtitle>{athlete.position}</CardSubtitle>
-                            <CardText>
+                            <h1>{athlete.name}</h1>
+                            <h3>{athlete.position}</h3>
+                            <p>
                               Medical Note :{athlete.medicalNote}
+                              <br />
                               Coaching Decision:
                               {athlete.coachingDecision}
-                            </CardText>
+                              <br />
+                            </p>
                             <Button>Button</Button>
-                          </CardBody>
+                          </div>
                         </Link>
                       </div>
-                    </Card>
-                  </CardDeck>
+                    </div>
+                  </div>
                 );
               })}
 
@@ -83,83 +86,151 @@ class AthleteList extends Component {
           </div>
         </div>
       );
-    } else if (this.props.theUser.staffingDivision === "Medical Staff") {
-      console.log("Yes");
+    } else if (this.props.theUser.staffingDivision === "MedStaff") {
+      console.log("AAAAAQAQAQAQAQAQAQAQAQAQAQAQAQAQAQA", this.state);
       return (
         <div>
+          wowowowowowowowowowow
           <div>
             {this.state.listOfAthletes &&
               this.state.listOfAthletes.map((athlete, index) => {
                 return (
-                  <div key={athlete._id}>
-                    <Link to={`/athletes/${athlete._id}`}>
-                      <img src={athlete.imgPath} />
-                      {/* <h4>{athlete.sport}</h4>
-                      <h5>{athlete.league}</h5> */}
-                      <h5>{athlete.team}</h5>
-                      <h5>{athlete.name}</h5>
-                      {/* <h5>{athlete.position}</h5> */}
-                      <h5>{athlete.currentInjuryStatus}</h5>
-                      <h4>{athlete.physicalMediatingFactorScore}</h4>
-                      <h5>{athlete.psychologicalMediatingFactorScore}</h5>
-                      <h5>{athlete.socialMediatingFactorScore}</h5>
-                      <h5>{athlete.physicalModeratingFactorScore}</h5>
-                      <h5>{athlete.psychologicalModeratingFactorScore}</h5>
-                      <h5>{athlete.socialModeratingFactorScore}</h5>
-                      <h5>{athlete.overallPhysiologicalHealth}</h5>
-                      <h5>{athlete.overallPsychologicalHealth}</h5>
-                      <h5>{athlete.overallSociologicalHealth}</h5>
-                      <h5>{athlete.injuryRiskScore}</h5>
-                      <h5>{athlete.medicalNote}</h5>
-                      <h5>{athlete.coachingDecision}</h5>
-                    </Link>
+                  <div class="row">
+                    <div class="col s12 m6">
+                      <div class="card">
+                        div key=
+                        {athlete._id}>
+                        <Link to={`/athletes/${athlete._id}`}>
+                          {athlete.currentInjuryStatus}
+                          <img src={athlete.imgPath} />
+                          <span class="card-title">{athlete.name}</span>
+                          <a class="btn-floating halfway-fab waves-effect waves-light red">
+                            <i class="material-icons">{athlete.position}</i>
+                          </a>
+                          <div class="card-content">
+                            <p>
+                              Medical Note :{athlete.medicalNote}
+                              <br />
+                              Coaching Decision:
+                              {athlete.coachingDecision}
+                              <br />
+                            </p>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
           </div>
-          {/* <div>
-            <AddAthlete getData={() => this.getAllAthletes()} />
-          </div> */}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div>
-            {this.state.listOfAthletes &&
-              this.state.listOfAthletes.map((athlete, index) => {
-                return (
-                  <div key={athlete._id}>
-                    <Link to={`/athletes/${athlete._id}`}>
-                      {/* <h4>{athlete.sport}</h4>
-                      <h5>{athlete.league}</h5>
-                      <h5>{athlete.team}</h5> */}
-                      <h5>{athlete.name}</h5>
-                      <h5>{athlete.position}</h5>
-                      <h5>{athlete.currentInjuryStatus}</h5>
-                      {/* <h4>{athlete.physicalMediatingFactorScore}</h4>
-                      <h5>{athlete.psychologicalMediatingFactorScore}</h5>
-                      <h5>{athlete.socialMediatingFactorScore}</h5>
-                      <h5>{athlete.physicalModeratingFactorScore}</h5>
-                      <h5>{athlete.psychologicalModeratingFactorScore}</h5>
-                      <h5>{athlete.socialModeratingFactorScore}</h5> */}
-                      {/* <h5>{athlete.overallPhysiologicalHealth}</h5>
-                      <h5>{athlete.overallPsychologicalHealth}</h5>
-                      <h5>{athlete.overallSociologicalHealth}</h5>
-                      <h5>{athlete.injuryRiskScore}</h5> */}
-                      <h5>{athlete.medicalNote}</h5>
-                      <h5>{athlete.coachingDecision}</h5>
-                    </Link>
-                  </div>
-                );
-              })}
-          </div>
-          {/* <div>
-            <AddAthlete getData={() => this.getAllAthletes()} />
-          </div> */}
         </div>
       );
     }
+
+    {
+      /* console.log("Yes");
+      return (
+        <div>
+          <div>
+            {this.state.listOfAthletes && */
+    }
+    {
+      /* //           this.state.listOfAthletes.map((athlete, index) => { */
+    }
+    {
+      /* //             return (
+    //               <div key={athlete._id}>
+    //                 <Link to={`/athletes/${athlete._id}`}>
+    //                   <img src={athlete.imgPath} />
+    //                   {/* <h4>{athlete.sport}</h4>
+    //                   <h5>{athlete.league}</h5> */
+    }
+    {
+      /* //                   <h5>{athlete.team}</h5>
+    //                   <h5>{athlete.name}</h5>
+    //                   {/* <h5>{athlete.position}</h5> */
+    }
+    {
+      /* //                   <h5>{athlete.currentInjuryStatus}</h5>
+    //                   <h4>{athlete.physicalMediatingFactorScore}</h4>
+    //                   <h5>{athlete.psychologicalMediatingFactorScore}</h5>
+    //                   <h5>{athlete.socialMediatingFactorScore}</h5>
+    //                   <h5>{athlete.physicalModeratingFactorScore}</h5>
+    //                   <h5>{athlete.psychologicalModeratingFactorScore}</h5>
+    //                   <h5>{athlete.socialModeratingFactorScore}</h5>
+    //                   <h5>{athlete.overallPhysiologicalHealth}</h5>
+    //                   <h5>{athlete.overallPsychologicalHealth}</h5>
+    //                   <h5>{athlete.overallSociologicalHealth}</h5>
+    //                   <h5>{athlete.injuryRiskScore}</h5>
+    //                   <h5>{athlete.medicalNote}</h5>
+    //                   <h5>{athlete.coachingDecision}</h5>
+    //                 </Link> */
+    }
+    {
+      /* //               </div>
+    //             );
+    //           })}
+    {/* //       </div> */
+      //       {/* <div> */}
+      /* //         <AddAthlete getData={() => this.getAllAthletes()} />
+    //       </div> */
+      //     </div> */}
+      //   );
+      // } else {
+      // return (
+      //   <div>
+      //     <div>
+      //       {this.state.listOfAthletes &&
+      //         this.state.listOfAthletes.map((athlete, index) => {
+      //           return (
+      //             <div key={athlete._id}>
+      //               <Link to={`/athletes/${athlete._id}`}>
+      {
+        /* <h4>{athlete.sport}</h4>
+                      <h5>{athlete.league}</h5>
+                      <h5>{athlete.team}</h5> */
+      }
+      {
+        /* <h5>{athlete.name}</h5>
+                      <h5>{athlete.position}</h5>
+                      <h5>{athlete.currentInjuryStatus}</h5> */
+      }
+      {
+        /* <h4>{athlete.physicalMediatingFactorScore}</h4>
+                      <h5>{athlete.psychologicalMediatingFactorScore}</h5>
+                      <h5>{athlete.socialMediatingFactorScore}</h5>
+                      <h5>{athlete.physicalModeratingFactorScore}</h5>
+                      <h5>{athlete.psychologicalModeratingFactorScore}</h5>
+                      <h5>{athlete.socialModeratingFactorScore}</h5> */
+      }
+      {
+        /* <h5>{athlete.overallPhysiologicalHealth}</h5>
+                      <h5>{athlete.overallPsychologicalHealth}</h5>
+                      <h5>{athlete.overallSociologicalHealth}</h5>
+                      <h5>{athlete.injuryRiskScore}</h5> */
+      }
+      //             <h5>{athlete.medicalNote}</h5>
+      //             <h5>{athlete.coachingDecision}</h5>
+      //           </Link>
+      //         </div>
+      //       );
+      //     })}
+      // </div>
+      {
+        /* <div>
+            <AddAthlete getData={() => this.getAllAthletes()} />
+          </div> */
+      }
+      //   </div>
+      // );
+      // }
+      // }
+
+      {
+        /* <input type="submit" value="Submit" />; */
+      }
+    }
   }
 }
+
 export default AthleteList;
